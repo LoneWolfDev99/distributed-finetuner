6931afac11718d5ba9f407abe87abf57fb3ed2ec
Aman <aman2023.btech@gmail.com>
2024-01-09 11:23:58 +0530

    decode base 64

--- helpers.py
+++ helpers.py
@@ -117,9 +117,10 @@ def load_custom_dataset(dataset_path):
         sys.exit(e)
 
 
-def decode_base64(text_to_encode):
-    encoded_text = base64.b64encode(text_to_encode.encode()).decode()
-    return encoded_text
+def decode_base64(encoded_string):
+    decoded_bytes = base64.b64decode(encoded_string)
+    decoded_text = decoded_bytes.decode('utf-8')
+    return decoded_text
 
 
 def gpu_memory():
